f8250a6cb7e771b0129d939156cca38b51e6d1d3
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2025-11-07 19:46:44 +0700

    proses: Controller

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -6,5 +6,59 @@ use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('prodi.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('prodi.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return redirect()->route('prodi.index')->with('success', 'Program Studi berhasil ditambahkan.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $prodi)
+    {
+        return view('prodi.show', compact('prodi'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $prodi)
+    {
+        return view('prodi.edit', compact('prodi'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $prodi)
+    {
+        return redirect()->route('prodi.index')->with('success', 'Program Studi berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $prodi)
+    {
+        return redirect()->route('prodi.index')->with('success', 'Program Studi berhasil dihapus.');
+    }
 }
